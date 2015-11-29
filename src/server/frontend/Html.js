7820129f73b11acfb52af0b66beaaf7615d58c83
Max GJ Panas <m@maxpanas.com>
2015-11-29 23:57:09 +0200

    fix typo in viewport meta

--- src/server/frontend/Html.js
+++ src/server/frontend/Html.js
@@ -12,7 +12,7 @@ export default class Html extends Component {
       <html lang={lang}>
         <head>
           <meta charSet="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale: 1" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
         </head>
         <body dangerouslySetInnerHTML={{__html: bodyHtml}} />
       </html>
