ad79d9b094a7881939c701e8a91e7ab6247704ed
Max GJ Panas <m@maxpanas.com>
2015-11-30 04:13:33 +0200

    add favicon and serve it

--- src/server/frontend/index.js
+++ src/server/frontend/index.js
@@ -1,11 +1,14 @@
+import path from 'path';
 import express from 'express';
 import compression from 'compression';
+import favicon from 'serve-favicon';
 import render from './render';
 
 const app = express();
 
 app.use(compression());
 
+app.use(favicon(path.join(__dirname + '../../../../assets/favicon/favicon.ico')));
 app.use('/assets/img', express.static('assets/img', {maxAge: '200d'}));
 app.use('/_assets', express.static('build', {maxAge: '200d'}));
 
