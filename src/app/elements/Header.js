34e36f01126058c73e6d7b68082f983a19263b88
Max GJ Panas <m@maxpanas.com>
2015-12-20 16:23:03 +0200

    First stab at styling the react components using inline styles

--- src/app/elements/Header.js
+++ src/app/elements/Header.js
@@ -1,14 +1,28 @@
 import React, {Component} from 'react';
 import {IndexLink, Link} from 'react-router';
 
+const style = {
+  logo: {
+    textTransform: 'uppercase',
+    textDecoration: 'none',
+    fontFamily: 'Arial Black, sans-serif',
+    fontWeight: 900,
+    fontSize: '72px',
+    color: 'tomato',
+  },
+};
+
 export default class Header extends Component {
   render() {
     return (
       <header>
-        <h1>MaxPanas.com</h1>
+        <h1>
+          <IndexLink to="/" style={style.logo}>
+            <abbr title="Max GJ Panas">MAX</abbr>
+          </IndexLink>
+        </h1>
         <nav>
           <ul>
-            <li><IndexLink to="/">Home</IndexLink></li>
             <li><Link to="/about">About Me</Link></li>
             <li><Link to="/asdasdasdas">404</Link></li>
           </ul>
