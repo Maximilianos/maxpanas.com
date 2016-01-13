e26b8f2b5f8774ea5f10c7f85305e4f7f1ed0afe
Max GJ Panas <m@maxpanas.com>
2016-01-14 01:13:00 +0200

    add article endpoint

---
+++ src/app/pages/Article.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import Entry from '../elements/Entry';
+
+export default class Article extends Component {
+  render() {
+    return (
+      <Entry title="Article" />
+    );
+  }
+}
--- src/app/routes.js
+++ src/app/routes.js
@@ -4,6 +4,7 @@ import {Route, IndexRoute} from 'react-router';
 import App from './App';
 import Home from './pages/Home';
 import About from './pages/About';
+import Article from './pages/Article';
 import NotFound from './pages/NotFound';
 
 export function createRoutes() {
@@ -11,6 +12,7 @@ export function createRoutes() {
     <Route path="/" component={App}>
       <IndexRoute component={Home} />
       <Route path="/about" component={About}/>
+	    <Route path="/:article" component={Article} />
       <Route path="*" component={NotFound}/>
     </Route>
   );
