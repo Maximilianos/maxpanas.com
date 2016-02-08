40d3438fa4bd0aed4db2b733d93a7fb1b561651f
Max GJ Panas <m@maxpanas.com>
2016-02-08 02:39:21 +0200

    add better error handling to article

--- src/app/containers/pages/Article.js
+++ src/app/containers/pages/Article.js
@@ -12,18 +12,50 @@ class Article extends Component {
     content: PropTypes.object
   };
 
+  static renderError({code, title, message}) {
+    return (
+      <Entry title={title}>
+        <Helmet title={title}/>
+        {message}
+      </Entry>
+    );
+  }
+
+  static renderGenericError() {
+    return Article.renderError({
+      code: 500,
+      title: 'Error 500',
+      message: 'Sorry there was a problem completing this request'
+    });
+  }
+
   render() {
-    const {content: {isFetching, data: {title, description, body}, error}} = this.props;
+    const {content: {
+      isFetching,
+      data: {title, description, body},
+      error
+    }} = this.props;
 
     if (isFetching) return <Entry title="Loading..."/>;
-    if (error || !title || !body) return <Entry title="Error">Sorry</Entry>;
+
+    if (error || !title || !body) {
+      if (error.response && error.response.status === 404) {
+        return Article.renderError({
+          code: 404,
+          title: 'Error 404',
+          message: 'Sorry there is no article at this url'
+        });
+      }
+
+      return Article.renderGenericError();
+    }
 
     return (
-      <Entry
-        title={title}
-        subtitle={description}
-      >
-        <Helmet title={title}/>
+      <Entry title={title} subtitle={description}>
+        <Helmet
+          title={title}
+          meta={[{name: 'description', content: description}]}
+        />
         <div dangerouslySetInnerHTML={{__html: body}}></div>
       </Entry>
     );
