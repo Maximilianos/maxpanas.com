6193ba25e3ca253142534e7e47413a1c7be6602f
Max GJ Panas <m@maxpanas.com>
2016-06-26 13:21:53 +0300

    implement sticky footer and add real site description

--- src/app/components/App/App.js
+++ src/app/components/App/App.js
@@ -8,13 +8,15 @@ import AppFooter from './AppFooter/AppFooter';
 import 'normalize.css';
 import '../../assets/fonts/filson-pro/webfonts.css';
 import '../../assets/fonts/avenir/webfonts.css';
+import './base.scss';
 import './App.scss';
 
 import favicon from '../../assets/favicon/favicon.ico'; // eslint-disable-line
 
 function App({children}) {
   return (
-    <div>
+    <div className="app">
+
       <NestedStatus code={200} />
       <Helmet
         link={[{
@@ -23,13 +25,23 @@ function App({children}) {
         }]}
         meta={[{
           name: 'description',
-          content: 'Developer blog'
+          content: 'My name is Max. I write code for the web. Sometimes ' +
+                   'I share the code that I\'ve written. Sometimes I ' +
+                   'give talks about code. This is my website. This is ' +
+                   'where I focus on writing about code.'
         }]}
         titleTemplate="%s - Max GJ Panas"
       />
-      <AppHeader />
-      {children}
-      <AppFooter />
+
+      <div className="app__content">
+        <AppHeader />
+        {children}
+      </div>
+
+      <div className="app__footer">
+        <AppFooter />
+      </div>
+
     </div>
   );
 }
