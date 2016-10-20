d5c499a3e4f5dc6f99d784287eab3cf59aacc60a
Max GJ Panas <m@maxpanas.com>
2016-10-20 16:13:41 +0100

    add morgan to log server requests

--- src/server/main.js
+++ src/server/main.js
@@ -1,11 +1,15 @@
 import express from 'express';
-
-import {port} from './config';
+import morgan from 'morgan';
 
 import frontend from './frontend';
 import errorHandler from './errors/errorHandler';
 
+import {port} from './config';
+
 const app = express();
+
+// enable logging
+app.use(morgan('dev'));
 
 app.use(frontend);
 app.use(errorHandler);
