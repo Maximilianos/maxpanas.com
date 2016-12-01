8a408f63bae8e93769f89767aaec0e984d75ad9a
Max GJ Panas <m@maxpanas.com>
2016-12-01 11:16:26 +0000

    add a basic handling of the articles route

--- src/server/api/content/index.js
+++ src/server/api/content/index.js
@@ -1,13 +1,25 @@
 import express from 'express';
 import fetch from 'isomorphic-fetch';
+import {
+  getArticlePath,
+  parseArticle
+} from './github';
 
 const app = express();
 
-app.get('/articles/:article', (req, res, next) => {
-
+app.get('/articles/:article', async (req, res) => {
+  try {
+    const response = await fetch(getArticlePath(req.params.article));
+    const json = await parseArticle()(response);
+    res.status(200).send(json);
+  } catch (error) {
+    res.status(500).send({
+      fuck: 'you'
+    });
+  }
 });
 
-app.get('/archives/:archive', (req, res, next) => {
+app.get('/archives/:archive', async (req, res) => {
 
 });
 
