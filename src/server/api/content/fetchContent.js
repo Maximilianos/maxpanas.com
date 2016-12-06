0a9581908ea2bc5b3f500115f73d52ed069bf515
Max GJ Panas <m@maxpanas.com>
2016-12-06 15:07:52 +0000

    refactor fetchContent utility to make it a bit easier to work with

--- src/server/api/content/fetchContent.js
+++ src/server/api/content/fetchContent.js
@@ -1,54 +1,5 @@
 import fetch from 'isomorphic-fetch';
 
-/**
- * Error messages for given status codes
- *
- * @type {{string}}
- */
-const errors = {
-  404: 'The requested content does not exist',
-  500: 'There was an error fetching the requested content'
-};
-
-
-/**
- * Fetch content from the given endpoint and
- * parse it with the given parser
- *
- * TODO: implement conditional requests https://developer.github.com/v3/#conditional-requests
- *
- * @param endpoint
- * @param parser
- * @returns {function(*=, *)}
- */
-export default function fetchContent({endpoint, parser}) {
-  return async (req, res) => {
-    try {
-      const response = await fetch(isFunc(endpoint) ? endpoint(req) : endpoint);
-      if (response.status !== 200) {
-        const status = response.status === 404 ? 404 : 500;
-        res.status(status).json({error: {
-          status,
-          message: errors[status]
-        }});
-        return;
-      }
-
-      const output = isFunc(parser)
-        ? await parser(response)
-        : response;
-
-      res.status(200).json(output);
-
-    } catch (error) {
-      res.status(500).json({error: {
-        status: 500,
-        message: errors[500]
-      }});
-    }
-  };
-}
-
 
 /**
  * Return whether a given thing is a function
@@ -59,4 +10,72 @@ export default function fetchContent({endpoint, parser}) {
  */
 function isFunc(thing) {
   return typeof thing === 'function';
+}
+
+
+/**
+ * Return the error response for the given status
+ * code
+ *
+ * @param {number} status
+ * @returns {{status: *, payload: {error: {status: *, message: *}}}}
+ */
+function fetchError(status) {
+  const errors = {
+    404: 'The requested content does not exist',
+    500: 'There was an error fetching the requested content'
+  };
+
+  return {
+    status,
+    payload: {error: {
+      status,
+      message: errors[status] || errors[500]
+    }}
+  };
+}
+
+
+/**
+ * Fetch content from the given endpoint and
+ * parse it with the given parser
+ *
+ * TODO: implement conditional requests https://developer.github.com/v3/#conditional-requests
+ *
+ * @param req
+ * @param endpoint
+ * @param parser
+ * @returns {*}
+ */
+async function fetchContent(req, {endpoint, parser}) {
+  try {
+    const response = await fetch(isFunc(endpoint) ? endpoint(req) : endpoint);
+    const {status} = response;
+    if (status !== 200) {
+      return fetchError(status === 404 ? 404 : 500);
+    }
+
+    const payload = isFunc(parser)
+      ? await parser(response)
+      : response;
+
+    return {status, payload};
+  } catch (error) {
+    return fetchError(500);
+  }
+}
+
+/**
+ * Respond to content request with fetched and
+ * parsed content
+ *
+ * @param endpoint
+ * @param parser
+ * @returns {function(*=, *)}
+ */
+export default function fetchContentHandler({endpoint, parser}) {
+  return async (req, res) => {
+    const {status, payload} = await fetchContent(req, {endpoint, parser});
+    res.status(status).json(payload);
+  };
 }
