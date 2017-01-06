c8ae7a7e469178ccc7f546ab12af87d0f1a0cdec
Max GJ Panas <m@maxpanas.com>
2017-01-06 17:48:05 +0000

    fix incorrect env setup check

--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,4 @@
-if (!process.env.NODE_ENV) {
+if (['development', 'production'].indexOf(process.env.NODE_ENV) === -1) {
   throw new Error(
     'Environment variable NODE_ENV must be ' +
     'set to development or production'
