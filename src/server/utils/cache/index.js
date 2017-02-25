a121667575c01583001de0a2f076c9e2451d2c8b
Max GJ Panas <m@maxpanas.com>
2017-02-25 20:05:18 +0000

    add a very basic connection retry strategy to the redis cache implementation

--- src/server/utils/cache/index.js
+++ src/server/utils/cache/index.js
@@ -42,7 +42,12 @@ export default function createCacheClient({
 }) {
   const lru = LRU(LRU_SIZE);
   const redis = createClient(REDIS_PATH, {
-    password: REDIS_PASSWORD
+    password: REDIS_PASSWORD,
+    retry_strategy({attempt, times_connected: timesConnected}) {
+      return timesConnected > 10
+        ? new Error('Redis connection failed too many times, not trying to connect any more')
+        : Math.min(attempt * 100, 3000);
+    }
   });
 
   redis.on('error', error => console.log(error.toString()));
